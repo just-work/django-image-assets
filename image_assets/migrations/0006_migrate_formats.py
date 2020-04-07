181c687110f24c64e30184305c05e60c80c652b0
tumb1er <zimbler@gmail.com>
2020-04-07 13:48:51 +0300

    Disable data migration for abstract asset type

--- image_assets/migrations/0006_migrate_formats.py
+++ image_assets/migrations/0006_migrate_formats.py
@@ -6,23 +6,23 @@ from image_assets import defaults
 
 # noinspection PyUnusedLocal
 def migrate_formats(apps, schema_editor):
-    app_label, model_name = defaults.ASSET_TYPE_MODEL.split('.')
     # noinspection PyPep8Naming
-    AssetType = apps.get_model(app_label, model_name)
-    formats = AssetType._meta.get_field('formats')
+    AssetType = apps.get_model('image_assets', 'AssetType')
 
     for at in AssetType.objects.all():
-        flag = getattr(formats, at.format)
+        flag = getattr(AssetType.formats, at.format)
         at.formats = flag
         at.save()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('image_assets', '0005_assettype_formats'),
     ]
 
-    operations = [
-        migrations.RunPython(migrate_formats, migrations.RunPython.noop),
-    ]
+    operations = []
+
+    if defaults.ASSET_TYPE_MODEL == 'image_assets.AssetType':
+        operations.extend([
+            migrations.RunPython(migrate_formats, migrations.RunPython.noop),
+        ])
